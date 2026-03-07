8732c53043238c37874e31122655e22749959e10
29veljace <raphael.kadainka@gmx.at>
2026-03-07 19:28:20 +0100

    Country.java ein bisschen vorbereitet

--- src/main/java/model/Country.java
+++ src/main/java/model/Country.java
@@ -1,5 +1,39 @@
 package model;
 
 public class Country {
-    // daten zu den ländern zb name, code, capital, population, blabla
+    private String name;
+    private String code;
+    private String capital;
+    private int population;
+    private int area;
+    // facts werde ich wrs string[] machen aber noch nicht sicher
+
+
+    public Country(String name, String code, String capital, int population, int area) {
+        this.name = name;
+        this.code = code;
+        this.capital = capital;
+        this.population = population;
+        this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
