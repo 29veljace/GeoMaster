48e5161d46bca020bc280b59461c00130b5c0e9e
29veljace <raphael.kadainka@gmx.at>
2026-05-05 09:08:16 +0200

    Variablen in Country.java renamed

--- src/main/java/model/Country.java
+++ src/main/java/model/Country.java
@@ -9,22 +9,44 @@ public class Country {
     private final String capital;
     private final int population;
     private final int area;
-    private final double avgHight;
+    private final double avgHeight;
     private final double bip;
     private final double highestPoint;
     private final double avgTemperature;
     private final Image flag;
 
+    public int getId() {
+        return id;
+    }
 
+    public double getAvgHeight() {
+        return avgHeight;
+    }
 
-    public Country(int id,String name, String code, String capital, int population, int area, double avgHight, double bip, double highestPoint, double avgTemperature,Image flag) {
+    public double getBip() {
+        return bip;
+    }
+
+    public double getHighestPoint() {
+        return highestPoint;
+    }
+
+    public double getAvgTemperature() {
+        return avgTemperature;
+    }
+
+    public Image getFlag() {
+        return flag;
+    }
+
+    public Country(int id, String name, String code, String capital, int population, int area, double avgHeight, double bip, double highestPoint, double avgTemperature, Image flag) {
         this.id = id;
         this.name = name;
         this.code = code;
         this.capital = capital;
         this.population = population;
         this.area = area;
-        this.avgHight = avgHight;
+        this.avgHeight = avgHeight;
         this.bip = bip;
         this.highestPoint = highestPoint;
         this.avgTemperature = avgTemperature;
