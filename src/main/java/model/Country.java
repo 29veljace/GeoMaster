4e6b6e751755e3bbc49e32a0111c6836147c9b5d
29veljace <raphael.kadainka@gmx.at>
2026-05-08 14:48:33 +0200

    fehler in Country.java verbessert + code für outline ist jetzt auch da

--- src/main/java/model/Country.java
+++ src/main/java/model/Country.java
@@ -13,6 +13,21 @@ public class Country {
     private double bip;
     private double avgTemperature;
     private Image flag;
+    private Image outline;
+
+    public Country(int id, String name, String code, String capital, int population, int area, double avgHeight, double bip, double avgTemperature, Image flag, Image outline) {
+        this.outline = outline;
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.capital = capital;
+        this.population = population;
+        this.area = area;
+        this.avgHeight = avgHeight;
+        this.bip = bip;
+        this.avgTemperature = avgTemperature;
+        this.flag = flag;
+    }
 
     public int getId() {
         return id;
@@ -39,19 +54,6 @@ public class Country {
         return flag;
     }
 
-    public Country(int id, String name, String code, String capital, int population, int area, double avgHeight, double bip, double avgTemperature, Image flag) {
-        setId(id);
-        setName(name);
-        setCode(code);
-        setCapital(capital);
-        setPopulation(population);
-        setArea(area);
-        setAvgHeight(avgHeight);
-        setBip(bip);
-        setAvgTemperature(avgTemperature);
-        setFlag(flag);
-    }
-
     public String getName() {
         return name;
     }
@@ -72,43 +74,11 @@ public class Country {
         return code;
     }
 
-    public void setFlag(Image flag) {
-        this.flag = flag;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setCapital(String capital) {
-        this.capital = capital;
-    }
-
-    public void setPopulation(int population) {
-        this.population = population;
-    }
-
-    public void setArea(int area) {
-        this.area = area;
-    }
-
-    public void setAvgHeight(double avgHeight) {
-        this.avgHeight = avgHeight;
-    }
-
-    public void setBip(double bip) {
-        this.bip = bip;
-    }
-
-    public void setAvgTemperature(double avgTemperature) {
-        this.avgTemperature = avgTemperature;
+    public Image getOutline() {
+        if (outline == null) {
+            outline = new Image(
+                    getClass().getResourceAsStream("/outlines/" + code + ".png"))
+        }
+        return outline;
     }
 }
