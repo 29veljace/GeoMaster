af7201e66e158fda164de292c9a722182ec6b287
29veljace <raphael.kadainka@gmx.at>
2026-06-11 09:31:27 +0200

    facts game fragen überarbeitet + NumberFormat

--- src/main/java/model/FactsGameModel.java
+++ src/main/java/model/FactsGameModel.java
@@ -1,14 +1,11 @@
 package model;
 
-
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import view.FactsGameView;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.text.NumberFormat;
+import java.util.*;
 
 public class FactsGameModel {
 
@@ -22,20 +19,21 @@ public class FactsGameModel {
     private boolean isC2 = false;
     private boolean isC3 = false;
     private boolean isC4 = false;
+    NumberFormat nf = NumberFormat.getInstance(Locale.GERMAN);
 
-    public void setCountries(){
+    public void setCountries() {
         factsGameModelCountryData.connect();
         System.out.println(factsGameModelCountryData.getConnection());
         c1 = factsGameModelCountryData.getCountry();
-        do{
+        do {
             c2 = factsGameModelCountryData.getCountry();
-        }while(c1.getId() == c2.getId());
-        do{
+        } while (c1.getId() == c2.getId());
+        do {
             c3 = factsGameModelCountryData.getCountry();
-        }while(c1.getId() == c3.getId() || c2.getId() == c3.getId());
-        do{
+        } while (c1.getId() == c3.getId() || c2.getId() == c3.getId());
+        do {
             c4 = factsGameModelCountryData.getCountry();
-        }while(c1.getId() == c4.getId() || c2.getId() == c4.getId() || c3.getId() == c4.getId());
+        } while (c1.getId() == c4.getId() || c2.getId() == c4.getId() || c3.getId() == c4.getId());
         System.out.println(c1.getName());
         System.out.println(c2.getName());
         System.out.println(c3.getName());
@@ -48,20 +46,20 @@ public class FactsGameModel {
     private final Set<Object> richtigeAntwortenC3 = Collections.synchronizedSet(new HashSet<>());
     private final Set<Object> richtigeAntwortenC4 = Collections.synchronizedSet(new HashSet<>());
     private final Set<Object> ausgewaehlt = new HashSet<>();
-    private final Button[] buttons = new Button[]{view.getBtn1(),view.getBtn2(),view.getBtn3(),view.getBtn4(),
-                                            view.getBtn5(),view.getBtn6(),view.getBtn7(),view.getBtn8(),
-                                            view.getBtn9(),view.getBtn10(),view.getBtn11(),view.getBtn12(),
-                                            view.getBtn13(),view.getBtn14(),view.getBtn15(),view.getBtn16()};
+    private final Button[] buttons = new Button[]{view.getBtn1(), view.getBtn2(), view.getBtn3(), view.getBtn4(),
+            view.getBtn5(), view.getBtn6(), view.getBtn7(), view.getBtn8(),
+            view.getBtn9(), view.getBtn10(), view.getBtn11(), view.getBtn12(),
+            view.getBtn13(), view.getBtn14(), view.getBtn15(), view.getBtn16()};
 
-    public FactsGameModel(){
+    public FactsGameModel() {
         setRichtigeAntworten();
     }
 
-    public void setRichtigeAntworten(){
+    public void setRichtigeAntworten() {
         setCountries();
         Random random = new Random();
 
-        Button[] buttons = new Button[] {
+        Button[] buttons = new Button[]{
                 view.getBtn1(), view.getBtn2(), view.getBtn3(), view.getBtn4(),
                 view.getBtn5(), view.getBtn6(), view.getBtn7(), view.getBtn8(),
                 view.getBtn9(), view.getBtn10(), view.getBtn11(), view.getBtn12(),
@@ -91,23 +89,54 @@ public class FactsGameModel {
                     Object hinzugefuegtesObjekt = null;
 
                     switch (eigenschaft) {
-                        case 0 -> { btn.setText(country.getName()); hinzugefuegtesObjekt = country.getName(); }
-                        case 1 -> { btn.setText(country.getCode()); hinzugefuegtesObjekt = country.getCode(); }
-                        case 2 -> { btn.setText(country.getCapital()); hinzugefuegtesObjekt = country.getCapital(); }
-                        case 3 -> { btn.setText(country.getPopulation() + " (pop.)"); hinzugefuegtesObjekt = btn.getText(); }
-                        case 4 -> { btn.setText(country.getArea() + " km²"); hinzugefuegtesObjekt = btn.getText(); }
-                        case 5 -> { btn.setText(country.getAvgHeight() + " cm"); hinzugefuegtesObjekt = btn.getText(); }
-                        case 6 -> { btn.setText(country.getBip() + " (Bip)"); hinzugefuegtesObjekt = btn.getText(); }
-                        case 7 -> { btn.setText(country.getAvgTemperature() + " °C"); hinzugefuegtesObjekt = btn.getText(); }
+                        case 0 -> {
+                            btn.setText(country.getName());
+                            hinzugefuegtesObjekt = country.getName();
+                        }
+                        case 1 -> {
+                            btn.setText(country.getCode());
+                            hinzugefuegtesObjekt = country.getCode();
+                        }
+                        case 2 -> {
+                            btn.setText(country.getCapital());
+                            hinzugefuegtesObjekt = country.getCapital();
+                        }
+                        case 3 -> {
+                            String einwohner = nf.format(country.getPopulation()) + " Einwohner";
+                            btn.setText(einwohner);
+                            hinzugefuegtesObjekt = einwohner;
+                        }
+                        case 4 -> {
+                            String flaeche = nf.format(country.getArea()) + " km² Fläche";
+                            btn.setText(flaeche);
+                            hinzugefuegtesObjekt = flaeche;
+                        }
+                        case 5 -> {
+                            String groesse = nf.format(country.getAvgHeight()) + "cm ⌀ Körpergröße";
+                            btn.setText(groesse);
+                            hinzugefuegtesObjekt = groesse;
+                        }
+                        case 6 -> {
+                            String bipText = gdpFormat(country.getBip()) + " $";
+                            btn.setText(bipText);
+                            hinzugefuegtesObjekt = bipText;
+                        }
+                        case 7 -> {
+                            String temp = nf.format(country.getAvgTemperature()) + " ⌀ °C";
+                            btn.setText(temp);
+                            hinzugefuegtesObjekt = temp;
+                        }
                         case 8 -> {
                             ImageView img = new ImageView(country.getFlag());
-                            img.setFitHeight(75); img.setFitWidth(100);
+                            img.setFitHeight(75);
+                            img.setFitWidth(100);
                             btn.setGraphic(img);
                             hinzugefuegtesObjekt = img;
                         }
                         case 9 -> {
                             ImageView img = new ImageView(country.getOutline());
-                            img.setFitHeight(75); img.setFitWidth(100);
+                            img.setFitHeight(75);
+                            img.setFitWidth(100);
                             btn.setGraphic(img);
                             hinzugefuegtesObjekt = img;
                         }
@@ -127,11 +156,10 @@ public class FactsGameModel {
     }
 
 
-
     public boolean auswaehlen(Object wert) {
         ausgewaehlt.add(wert);
 
-        if(ausgewaehlt.size() == 1) {
+        if (ausgewaehlt.size() == 1) {
             if (richtigeAntwortenC1.containsAll(ausgewaehlt)) {
                 isC1 = true;
             }
@@ -145,16 +173,16 @@ public class FactsGameModel {
                 isC4 = true;
             }
         }
-        if(isC1){
+        if (isC1) {
             return richtigeAntwortenC1.containsAll(ausgewaehlt);
         }
-        if(isC2){
+        if (isC2) {
             return richtigeAntwortenC2.containsAll(ausgewaehlt);
         }
-        if(isC3){
+        if (isC3) {
             return richtigeAntwortenC3.containsAll(ausgewaehlt);
         }
-        if(isC4){
+        if (isC4) {
             return richtigeAntwortenC4.containsAll(ausgewaehlt);
         }
         return false;
@@ -166,7 +194,7 @@ public class FactsGameModel {
         if ((ausgewaehlt.containsAll(richtigeAntwortenC1) || ausgewaehlt.containsAll(richtigeAntwortenC2)
                 || ausgewaehlt.containsAll(richtigeAntwortenC3) || ausgewaehlt.containsAll(richtigeAntwortenC4))
                 && (ausgewaehlt.size() == richtigeAntwortenC1.size() || ausgewaehlt.size() == richtigeAntwortenC2.size()
-                || ausgewaehlt.size() == richtigeAntwortenC3.size() || ausgewaehlt.size() == richtigeAntwortenC4.size())){
+                || ausgewaehlt.size() == richtigeAntwortenC3.size() || ausgewaehlt.size() == richtigeAntwortenC4.size())) {
             isC1 = false;
             isC2 = false;
             isC3 = false;
@@ -185,7 +213,6 @@ public class FactsGameModel {
     }
 
 
-
     public FactsGameModelCountryData getFactsGameModelCountryData() {
         return factsGameModelCountryData;
     }
@@ -194,6 +221,15 @@ public class FactsGameModel {
         return ausgewaehlt;
     }
 
+    private String gdpFormat(double gdp) {
+        java.text.NumberFormat nf = java.text.NumberFormat.getInstance(java.util.Locale.GERMAN);
+        if (gdp >= 1_000_000_000) {
+            double milliarden = gdp / 1_000_000_000.0;
+            nf.setMaximumFractionDigits(2);
+            return nf.format(milliarden) + " Mrd. BIP";
+        }
+        return nf.format(gdp) + " BIP";
+    }
 }
 
 
